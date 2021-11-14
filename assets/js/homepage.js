d12102d270ea40e344d1b88a64f38d14b61f86d9
phopps <pathopps1@gmail.com>
2021-11-14 13:14:23 -0500

    6-1-6 complete updated request logic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,15 @@
-let getUserRepos = function() {
-  let response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-    console.log("inside", response);
+let user = prompt("User: ");
 
+let getUserRepos = function(user) {
+  // format the github api url
+  let apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
   });
-
-  console.log("outside");
 };
 
-getUserRepos();
+getUserRepos(user);
